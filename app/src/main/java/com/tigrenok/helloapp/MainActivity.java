ae1bdf9ac9e890e714e45aaedb0e23bab6eab51d
Rzayev Metin <rzayevmetin@mail.ru>
2023-10-14 12:08:01 +0500

    delete comment

--- app/src/main/java/com/tigrenok/helloapp/MainActivity.java
+++ app/src/main/java/com/tigrenok/helloapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.tigrenok.helloapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // получаем элемент textVie
     }
 }
